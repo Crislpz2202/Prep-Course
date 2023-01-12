44514740c444aa6a88a5f0ce8b733c19e605073c
Crislpz2202 <crislpz2202@gmail.com>
2023-01-11 22:46:11 -0500

    tarearealizada

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -156,6 +156,7 @@ function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
   return Math.random ();
+
 }
 
 function esPositivo(numero) {
@@ -278,3 +279,7 @@ module.exports = {
   deEuroAdolar,
   esVocal,
 };
+
+
+
+
